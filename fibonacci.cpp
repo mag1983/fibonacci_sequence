094791bb88fa477662eb10919c57a892fb7f93b2
mag1983 <31274035+mag1983@users.noreply.github.com>
2017-10-27 13:47:43 +0200

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+//The Fibonacci sequence
+#include <iostream>
+
+using namespace std;
+int n,first=0,second=1,next;
+
+int main()
+{
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    cout <<"\n  The Fibonacci sequence is:";
+    for (int i=0; i<n ;i++) {           										//loop works fine till the condition is TRUE
+        cout<<" "<<first;
+        next=first+second;
+        first=second;
+        second=next;
+    }
+    cout<<"\n\n  "<<n<<" term value is: "<<first<<endl; 	//first value when the loop condition is FALSE
+    return 0;
+}
